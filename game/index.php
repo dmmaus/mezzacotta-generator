e450e22a6b8127481b249b80e79b5bee61abc998
Andrew Coker <andoru.c@gmail.com>
2019-07-10 16:17:23 +1000

    added board game generator

---
+++ game/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<title>Random Board Game Descriptions - mezzacotta</title>
+</head>
+<body>
+<h1>Random Tavern names</h1>
+<p>
+5 random board game descriptions. Reload for more.
+</p>
+<ul>
+<li>
+<?php
+$command = escapeshellcmd('/usr/bin/python ../generique.py game/base 5');
+$output = shell_exec($command);
+$output = preg_replace('/\n/', "</li>\n<li>", rtrim($output));
+echo $output;
+?>
+</li>
+</ul>
+</body>
+</html>
