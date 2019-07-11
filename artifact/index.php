581555eded0600b85c4fbd4e19ce5495e505d0b2
Andrew Coker <andoru.c@gmail.com>
2019-07-11 11:21:37 +1000

    Added artifact generators, with vocab files of transitive verbs and artifact nouns and adjectives.

---
+++ artifact/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<title>Random Artifacts - mezzacotta</title>
+</head>
+<body>
+<h1>Random Artifacts</h1>
+<p>
+10 random artifacts, perfect for your party of adventurers to be searching for. Reload for more.
+</p>
+<ul>
+<li>
+<?php
+$command = escapeshellcmd('/usr/bin/python ../generique.py artifact/base 10');
+$output = shell_exec($command);
+$output = preg_replace('/\n/', "</li>\n<li>", rtrim($output));
+echo $output;
+?>
+</li>
+</ul>
+</body>
+</html>
