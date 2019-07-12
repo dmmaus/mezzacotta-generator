ad7ffc8c723a490f951bc974ed3b5571e512acef
David Morgan-Mar <dmm@dangermouse.net>
2019-07-13 08:23:35 +1000

    Add image file and coloured background.

--- tavern/index.php
+++ tavern/index.php
@@ -3,8 +3,15 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 <title>Random Tavern Names - mezzacotta</title>
+<style>
+body {
+  background-color: #E4F8F8;
+}
+</style>
 </head>
+
 <body>
+<img src="tavern.png" width="813" height="320" alt="[tavern image]" srcset="tavern_2x.png 2x" align="right">
 <h1>Random Tavern names</h1>
 <p>
 10 random tavern names. Reload for more.
@@ -20,5 +27,9 @@ echo $output;
 ?>
 </li>
 </ul>
+
+<p>
+(Tavern art: <a href="https://creativecommons.org/licenses/by/3.0/deed.en">Creative Commons Attribution 3.0 Unported</a> by <a href="https://commons.wikimedia.org/wiki/File:Bridge-medieval-fantasy-city.png">David Revoy</a>.)
+</p>
 </body>
 </html>
