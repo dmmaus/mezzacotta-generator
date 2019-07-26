e5d5fd8e5d6093933f339021e7691e95d39631d9
David Morgan-Mar <dmm@dangermouse.net>
2019-07-26 17:18:15 +1000

    Add CSS formatting to movie generator. List all generators on home page.

--- index.php
+++ index.php
@@ -10,10 +10,12 @@
 A generic, extensible, customisable random text generator. The code is <a href="https://github.com/dmmaus/mezzacotta-generator">open source on Github</a>, and we welcome contributions via pull requests.
 </p>
 <ul>
-<li><a href="tavern/">Random fantasy tavern names</a></li>
-<li><a href="artifact/">Random fantastic artifacts</a></li>
-<li><a href="village/">Random village names</a></li>
-<li><a href="dish/">mezzacotta Café, version 2</a> - the reincarnation of the original <a href="/mezzacafe/">mezzacotta Café</a> with all-new code. Some HTML formatting work still to be done.</li>
+<li><a href="tavern/">Fantasy tavern names</a></li>
+<li><a href="artifact/">Fantastic artifacts</a></li>
+<li><a href="game/">Board game descriptions</a></li>
+<li><a href="village/">Village names and festival descriptions</a></li>
+<li><a href="dish/">mezzacotta Café, version 2</a> - the reincarnation of the original <a href="/mezzacafe/">mezzacotta Café</a> with all-new code. HTML formatting work still to be done.</li>
+<li><a href="movie/">mezzacotta Cinématique, version 2</a> - the reincarnation of the original <a href="/cinematique/">mezzacotta Cinématique</a> with all-new code. HTML formatting work still to be done.</li>
 </ul>
 </body>
 </html>
