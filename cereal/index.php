56a7a6a415cb846952916bdc0d92a8efbc444d61
David Morgan-Mar <dmm@dangermouse.net>
2019-08-04 12:31:55 +1000

    Add new breakfast cereal generator.

---
+++ cereal/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<title>Random breakfast cereals - mezzacotta</title>
+</head>
+<body>
+<h1>Random Breakfast Cereals</h1>
+<p>
+10 random breakfast cereals, perfect for starting your day with a burst of energy. Reload for more.
+</p>
+<ul>
+<?php
+$command = escapeshellcmd('/usr/bin/python ../generique.py cereal/name cereal/description 10');
+$output = shell_exec($command);
+$output = preg_replace("/'/", "’", $output);
+$lines = explode("\n", rtrim($output));
+foreach ($lines as $line)
+{
+    $parts = explode("~~", $line);
+    $name = trim($parts[0]);
+    $description = trim($parts[1]);
+    echo "<li>$name<br><i>$description</i></li>\n<br>\n";
+}
+?>
+</ul>
+
+<!-- footer -->
+<hr>
+<p>
+<i><a href="../">mezzacotta Random Generators Home</a></i>
+</p>
+
+</body>
+</html>
