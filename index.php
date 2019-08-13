655f68d7848e90527725e54371626f6ac1a556a0
Andrew Coker <andoru.c@gmail.com>
2019-08-13 10:45:26 +1000

    vocab updates

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ A generic, extensible, customisable random text generator. The code is <a href="
 <li><a href="artifact/">Fantastic artifacts</a></li>
 <li><a href="village/">Village names and festival descriptions</a></li>
 <li><a href="insult/">Mockery Most Vicious</a> - fantasy insults, perfect for when your <i>D&amp;D</i> bard unleashes Vicious Mockery</li>
+<li><a href="law/">Random weird laws from around the world</a></li>
 <br>
 <li><a href="game/">Board game descriptions</a></li>
 <li><a href="dish/">mezzacotta Café version 2</a> - the reincarnation of the original <a href="/mezzacafe/">mezzacotta Café</a> with all-new code.</li>
