bb7d74055c2b4496dd2307c664872cec0a859529
Andrew Coker <andoru.c@gmail.com>
2019-10-17 09:26:39 +1100

    added app generator

--- index.php
+++ index.php
@@ -10,13 +10,14 @@
 A generic, extensible, customisable random text generator. The code is <a href="https://github.com/dmmaus/mezzacotta-generator">open source on Github</a>, and we welcome contributions via pull requests.
 </p>
 <ul>
-<li><a href="tavern/">Fantasy tavern names</a></li>
 <li><a href="artifact/">Fantastic artifacts</a></li>
 <li><a href="village/">Village names and festival descriptions</a></li>
-<li><a href="insult/">Mockery Most Vicious</a> - fantasy insults, perfect for when your <i>D&amp;D</i> bard unleashes Vicious Mockery</li>
 <li><a href="law/">Random weird laws from around the world</a></li>
-<br>
 <li><a href="game/">Board game descriptions</a></li>
+<li><a href="app/">Random ideas for new apps</a></li>
+<br>
+<li><a href="tavern/">Fantasy tavern names</a></li>
+<li><a href="insult/">Mockery Most Vicious</a> - fantasy insults, perfect for when your <i>D&amp;D</i> bard unleashes Vicious Mockery</li>
 <li><a href="dish/">mezzacotta Café version 2</a> - the reincarnation of the original <a href="/mezzacafe/">mezzacotta Café</a> with all-new code.</li>
 <li><a href="movie/">mezzacotta Cinématique version 2</a> - the reincarnation of the original <a href="/cinematique/">mezzacotta Cinématique</a> with all-new code.</li>
 </ul>
