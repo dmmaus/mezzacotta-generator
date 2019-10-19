ebe2fd18dac456e2517eb230ba0f1fb623b95d7a
David Morgan-Mar <dmm@dangermouse.net>
2019-10-19 11:11:20 +1100

    Reconfigure list of generators.

--- index.php
+++ index.php
@@ -9,6 +9,15 @@
 <p>
 A generic, extensible, customisable random text generator. The code is <a href="https://github.com/dmmaus/mezzacotta-generator">open source on Github</a>, and we welcome contributions via pull requests.
 </p>
+<p><b>Mature generators</b></p>
+<ul>
+<li><a href="tavern/">Fantasy tavern names</a></li>
+<li><a href="insult/">Mockery Most Vicious</a> - fantasy insults, perfect for when your <i>D&amp;D</i> bard unleashes Vicious Mockery</li>
+<li><a href="dish/">mezzacotta Café version 2</a> - the reincarnation of the original <a href="/mezzacafe/">mezzacotta Café</a> with all-new code.</li>
+<li><a href="movie/">mezzacotta Cinématique version 2</a> - the reincarnation of the original <a href="/cinematique/">mezzacotta Cinématique</a> with all-new code.</li>
+</ul>
+
+<p><b>Work in progress</b></p>
 <ul>
 <li><a href="artifact/">Fantastic artifacts</a></li>
 <li><a href="village/">Village names and festival descriptions</a></li>
@@ -16,11 +25,7 @@ A generic, extensible, customisable random text generator. The code is <a href="
 <li><a href="game/">Board game descriptions</a></li>
 <li><a href="app/">Random ideas for new apps</a></li>
 <li><a href="bird/">Random bird names</a></li>
-<br>
-<li><a href="tavern/">Fantasy tavern names</a></li>
-<li><a href="insult/">Mockery Most Vicious</a> - fantasy insults, perfect for when your <i>D&amp;D</i> bard unleashes Vicious Mockery</li>
-<li><a href="dish/">mezzacotta Café version 2</a> - the reincarnation of the original <a href="/mezzacafe/">mezzacotta Café</a> with all-new code.</li>
-<li><a href="movie/">mezzacotta Cinématique version 2</a> - the reincarnation of the original <a href="/cinematique/">mezzacotta Cinématique</a> with all-new code.</li>
+<li><a href="cereal/">Random breakfast cereals</a></li>
 </ul>
 </body>
 </html>
