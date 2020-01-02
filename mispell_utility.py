d1cbb5062087250403132c713f31c1dba67ca033
Andrew Coker <andoru.c@gmail.com>
2020-01-02 20:36:49 +1100

    fix utility

--- mispell_utility.py
+++ mispell_utility.py
@@ -6,6 +6,6 @@ res = generique.generate(['band/base', '5'])
 
 for word in res:
     word = word.lower()
-    m = mispell.mispell(word)
+    m = mispell.mispell(word).strip()
     print('' if m == word else '###', word, '->', m)
 
