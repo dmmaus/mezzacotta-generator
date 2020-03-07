7e4ccdc324c1fca423a2533c7c80e442efde46de
David Morgan-Mar <dmm@dangermouse.net>
2020-03-07 19:57:26 +1100

    Add header codes to force browsers not to cache pages.

--- insult/index.php
+++ insult/index.php
@@ -1,3 +1,9 @@
+<?php
+    // set non-caching headers so browser will reload random comic each time URL is loaded
+    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+    header("Cache-Control: no-cache");
+    header("Pragma: no-cache");
+?>
 <!DOCTYPE html>
 <html>
 <head>
