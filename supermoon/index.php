39670472cafb0d6157aed98625dd7187930c6492
David Morgan-Mar <dmm@dangermouse.net>
2022-07-15 20:31:01 +1000

    Add supermoon generator

---
+++ supermoon/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<title>Random Supermoons - mezzacotta</title>
+</head>
+<body>
+<h1>Random Supermoons</h1>
+<p>
+10 random supermoons. Reload for more.
+</p>
+<ul>
+<?php
+putenv("PYTHONIOENCODING=UTF-8");
+chdir('..');
+$command = escapeshellcmd('/usr/bin/python3 generique.py supermoon/moon 10');
+$output = shell_exec($command);
+$output = preg_replace("/'/", "’", $output);
+$output = preg_replace("/&/", "&amp;", $output);
+$output = preg_replace("/</", "&lt;", $output);
+$output = preg_replace("/>/", "&gt;", $output);
+$lines = explode("\n", rtrim($output));
+foreach ($lines as $line)
+{
+    echo "<li>$line</i></li>\n";
+}
+?>
+</ul>
+
+<!-- footer -->
+<hr>
+<p>
+<i><a href="../">mezzacotta Random Generators Home</a></i>
+</p>
+
+</body>
+</html>
